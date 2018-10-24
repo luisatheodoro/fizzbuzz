fd48235580b77c400de0115ac69d3c0be6baf3f2
luisatheodoro <theodoro.luisa@gmail.com>
2018-10-24 14:11:51 +0000

    refactoring fizzbuzz spec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,35 +3,19 @@ require 'fizzbuzz'
 numbers = (1..100)
 describe 'fizzbuzz' do
   it 'returns "Fizz" when passed numbers divisible by 3' do
-    numbers.each do |n|
-      if n % 3 == 0 && n % 5 != 0
-        expect(fizzbuzz(n)).to eq 'Fizz'
-      end
-    end
+    numbers.each {|n| expect(fizzbuzz(n)).to eq 'Fizz' if n % 3 == 0 && n % 5 != 0}
   end
   
   it 'returns "Buzz" when passed numbers divisible by 5' do
-    numbers.each do |n|
-      if n % 3 != 0 && n % 5 == 0
-        expect(fizzbuzz(n)).to eq 'Buzz'
-      end
-    end
+    numbers.each {|n| expect(fizzbuzz(n)).to eq 'Buzz' if n % 3 != 0 && n % 5 == 0}
   end
   
   it 'returns "FizzBuzz" when passed numbers divisible by 5 and by 3' do
-    numbers.each do |n|
-      if n % 3 == 0 && n % 5 == 0
-        expect(fizzbuzz(n)).to eq 'FizzBuzz'
-      end
-    end
+    numbers.each {|n| expect(fizzbuzz(n)).to eq 'FizzBuzz' if n % 3 == 0 && n % 5 == 0}
   end
   
   it 'returns the number when passed numbers not divisible by 5 and by 3' do
-    numbers.each do |n|
-      if n % 3 != 0 && n % 5 != 0
-        expect(fizzbuzz(n)).to eq n
-      end
-    end
+    numbers.each {|n| expect(fizzbuzz(n)).to eq n if n % 3 != 0 && n % 5 != 0}
   end
   
 end
