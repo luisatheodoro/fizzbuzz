e137dc1b8719d301de4124d33b877f4b677633e5
luisatheodoro <theodoro.luisa@gmail.com>
2018-10-25 12:56:22 +0000

    refactoring code and commenting solution for the number to call the method instead of being passed as an argument

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,15 @@
 def fizzbuzz(number)
-  if number % 15 == 0
-    return 'FizzBuzz'
-  elsif number % 5 == 0
-    return 'Buzz'
-  elsif number % 3 == 0
-    return  'Fizz'
-  else
-    return  number
-  end
+  number % 15 == 0 ? 'FizzBuzz' : number % 5 == 0 ? 'Buzz' : number % 3 == 0 ? 'Fizz' : number
 end
+
+#To be able to call the number on the method it would have to be a public method, so any object could call it. 
+#And passing the self as the number that is calling the method, because it is calling itself. :
+# public
+# def fizzbuzz
+#   self % 15 == 0 ? 'FizzBuzz' : self % 5 == 0 ? 'Buzz' : self % 3 == 0 ? 'Fizz' : self
+#   # if self == 3
+#     # 'Fizz'
+#   # elsif self == 5
+#     # 'Buzz'
+#   # end
+# end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,20 +2,25 @@ require 'fizzbuzz'
 
 numbers = (1..100)
 describe 'fizzbuzz' do
-  it 'returns "Fizz" when passed numbers divisible by 3' do
-    numbers.each {|n| expect(fizzbuzz(n)).to eq 'Fizz' if n % 3 == 0 && n % 5 != 0}
+
+  it 'returns "Fizz" when passing numbers divisible by 3' do
+    numbers.each {|n| expect(fizzbuzz(n)).to eq 'Fizz' if n % 3 == 0 && n % 5 != 0 }
   end
   
-  it 'returns "Buzz" when passed numbers divisible by 5' do
+  it 'returns "Buzz" when passing numbers divisible by 5' do
     numbers.each {|n| expect(fizzbuzz(n)).to eq 'Buzz' if n % 3 != 0 && n % 5 == 0}
   end
   
-  it 'returns "FizzBuzz" when passed numbers divisible by 5 and by 3' do
+  it 'returns "FizzBuzz" when passing numbers divisible by 5 and by 3' do
     numbers.each {|n| expect(fizzbuzz(n)).to eq 'FizzBuzz' if n % 3 == 0 && n % 5 == 0}
   end
   
-  it 'returns the number when passed numbers not divisible by 5 and by 3' do
+  it 'returns the number when passing numbers not divisible by 5 and by 3' do
     numbers.each {|n| expect(fizzbuzz(n)).to eq n if n % 3 != 0 && n % 5 != 0}
   end
   
+  # if the number would call the method fizzbuzz it would be like this:
+  # it 'returns "fizz" for the number 3' do
+  #   expect(3.fizzbuzz).to eq 'fizz'
+  # end
 end
